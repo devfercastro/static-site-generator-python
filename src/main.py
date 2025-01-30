0b74effc33fd0bc8855a6c140665d86122d42639
Fernando Castro <devfercastro@gmail.com>
2025-01-30 15:30:03 -0300

    CH2: Nodes L1: TextNode
    - Add shell script to run project code
    - Ignore __pychache__/
    - Implement TextType enum
    - Implement TextNode class

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    dummy_obj = TextNode("fañsdkljfa", TextType.BOLD, "https://nose")
+    print(dummy_obj)
+
+
+if __name__ == "__main__":
+    main()
