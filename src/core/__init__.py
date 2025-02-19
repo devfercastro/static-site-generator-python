41f00681334a57d220646793ed63bbe6c51a9cc1
Fernando Castro <devfercastro@gmail.com>
2025-02-19 20:35:29 -0300

    fix(core): missing enum `TextType`

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,8 +1,8 @@
 from .htmlnode import HTMLNode
 from .leafnode import LeafNode
 from .parentnode import ParentNode
-from .textnode import TextNode, TextType
-from .types import BlockType
+from .textnode import TextNode
+from .types import BlockType, TextType
 
 __all__ = ['HTMLNode', 'LeafNode', 'ParentNode',
            'TextNode', 'TextType', 'BlockType']
--- src/core/types.py
+++ src/core/types.py
@@ -8,3 +8,12 @@ class BlockType(Enum):
     UNORDERED_LIST = "unordered_list"
     ORDERED_LIST = "ordered_list"
     PARAGRAPH = "paragraph"
+
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
