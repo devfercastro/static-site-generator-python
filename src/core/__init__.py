dd24840085c1fe3caa918bf0c59630ec3de855bb
Fernando Castro <devfercastro@gmail.com>
2025-02-19 20:22:05 -0300

    refactor(core): new file to save all enums

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -2,5 +2,7 @@ from .htmlnode import HTMLNode
 from .leafnode import LeafNode
 from .parentnode import ParentNode
 from .textnode import TextNode, TextType
+from .types import BlockType
 
-__all__ = ['HTMLNode', 'LeafNode', 'ParentNode', 'TextNode', 'TextType']
+__all__ = ['HTMLNode', 'LeafNode', 'ParentNode',
+           'TextNode', 'TextType', 'BlockType']
