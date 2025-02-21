fe5a69dad51e902a4785ff10e527f58d6f206469
Fernando Castro <devfercastro@gmail.com>
2025-02-21 18:49:56 -0300

    refactor(main): ruff stuffs

--- src/main.py
+++ src/main.py
@@ -1,18 +1,26 @@
-import os
-from core import TextNode, TextType
-from utils import sync_directories
+"""Just the main file."""
+
+from pathlib import Path
+
+from .core import TextNode, TextType
+from .utils import sync_directories
 
 
 def main():
+    """Execute the program.
+
+    This function creates a dummy TextNode object and synchronizes
+    the contents of the 'static' directory to the 'public' directory.
+    """
     dummy_obj = TextNode("fañsdkljfa", TextType.BOLD, "https://nose")
-    print(dummy_obj)
+    print(dummy_obj)  # noqa: T201
 
 
 if __name__ == "__main__":
     main()
 
     # make sure "public" folder exist
-    destination = "public"
-    os.makedirs(os.path.join(os.path.dirname("src"), destination), exist_ok=True)
+    destination = Path("public")
+    destination.mkdir(parents=True, exist_ok=True)
 
-    sync_directories("static", destination)
+    sync_directories(Path("static"), destination)
