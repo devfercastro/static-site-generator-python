014cd08fb465dc3ab496a70765aaaacc908f8077
Fernando Castro <devfercastro@gmail.com>
2025-03-06 15:54:43 -0300

    fix(markdown): erase code that removes indentation inside blocks

--- src/markdown/block_parser.py
+++ src/markdown/block_parser.py
@@ -27,15 +27,7 @@ def markdown_to_blocks(markdown: str) -> List[str]:
         stripped_block = block.strip()
 
         if stripped_block != "":
-            # handle blocks with multiple indentated lines
-            if "\n" in stripped_block:
-                # remove indentation from each line
-                outdented_lines = [line.strip() for line in stripped_block.split("\n")]
-                blocks.append("\n".join(outdented_lines))
-
-            else:
-                # simple blocks just appends it
-                blocks.append(stripped_block)
+            blocks.append(stripped_block)
 
     return blocks
 
